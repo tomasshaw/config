b66fab09592fb4a7f52fa7818b490747a40205f7
Thomas Shaw <tomasshaw@outlook.com>
2023-10-26 17:26:28 -0300

    feat(null-ls): add format on save

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -1,34 +1,49 @@
 local present, null_ls = pcall(require, "null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 if not present then
-  return
+	return
 end
 
-local b = null_ls.builtins
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 local sources = {
 
-  -- formatting
-  b.formatting.clang_format,
-  -- b.formatting.deno_fmt.with {
-  --   filetypes = { "json" },
-  -- },
-  b.formatting.stylua,
-  b.formatting.prettier,
-  -- b.formatting.prettier_eslint,
-  b.formatting.eslint,
-  -- b.formatting.stylelint,
+	-- formatting
+	formatting.clang_format,
+	-- b.formatting.deno_fmt.with {
+	--   filetypes = { "json" },
+	-- },
+	formatting.stylua,
+	formatting.prettier,
+	-- b.formatting.prettier_eslint,
+	formatting.eslint,
+	-- b.formatting.stylelint,
 
-  -- diagnostics
-  b.diagnostics.eslint,
-  b.diagnostics.tsc,
-  -- b.diagnostics.stylelint,
+	-- diagnostics
+	diagnostics.eslint,
+	diagnostics.tsc,
+	-- b.diagnostics.stylelint,
 
-  -- code_actions
-  b.code_actions.eslint,
+	-- code_actions
+	code_actions.eslint,
 }
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ timeout_ms = 5000 })
+				end,
+			})
+		end
+	end,
+})
