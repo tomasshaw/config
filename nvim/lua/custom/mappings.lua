efbb22f13b4d75cc1121bee59cda4817d656120a
Thomas Shaw <tomasshaw@outlook.com>
2023-10-26 16:55:40 -0300

    fix(mappings): <leader> + mp now correctly shows :MarkDownPreview

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -4,15 +4,10 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>mp"] = { ":MarkdownPreviewToggle <CR>", "Toggle Markdown Preview" },
   },
 }
 
-M.markdown = {
-  plugin = true,
-  n = {
-    ["<leader>mp"] = { ":MarkdownPreviewToggle", "Toggle Markdown Preview" },
-  },
-}
 -- more keybinds!
 
 return M
